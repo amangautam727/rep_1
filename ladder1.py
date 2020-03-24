5c89d6cc2554bc12125f0ffa3886029e3af58967
amangautam727 <62521188+amangautam727@users.noreply.github.com>
2020-03-24 21:59:55 +0530

    Add files via upload

---
+++ ladder1.py
@@ -0,0 +1,12 @@
+-
+n=int(input("enter the no"))
+for j in range (n):
+  for i in range (5):
+    if i in{0,2}:
+      print('*'+(' '*4)+'*')
+    elif i in {4}:
+      print('*'*6)
+    else:
+      print(' ')
+    
+    v  v    wwi//a/-
